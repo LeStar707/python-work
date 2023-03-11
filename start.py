b63e7fd79ea56883cac075cd560ac3532f0237e6
Pavlo <no@no.com>
2023-03-11 13:21:11 +0200

    Розсортували тести та пакети

--- start.py
+++ start.py
@@ -1,1 +1,18 @@
-print('Hello')
+import pandas as pd
+
+# Завантажуємо дані з Excel файлу
+data = pd.read_excel('Paket.xlsx')
+report = pd.read_excel('2023-02-copy.xlsx')
+# Виводимо перші 5 рядків даних
+# print(data.head())
+# print(report.head())
+# print(report["Код Анализа"])
+
+for paket_id in report["Код Анализа"]:
+
+    if paket_id in data:
+        print("Це пакет")
+    else:
+        for test_id in data["№ Теста IQLab"]:
+            if test_id == paket_id:
+                print("Це тест")
